f452a73e27a8a27071bd2e5cef13df67998f9e26
Lilia Arelize <Liliaarelizetorres@gmail.com>
2021-03-08 16:55:54 -0500

    madlibs for both javascript and python

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = input("Gimme a noun...") #this version includes user-submotted values
+adjective = input("Gimme an adjetive...")
+adverb = input("Gimme an adverb...")
+verb = input("Gimme a verb...")
+
+print("On a tropical " + str(noun) + ",")
+print("Underneath a " + str(adjective) + " lava moon,")
+print("Hangin' " + str(adverb) + " with the hulu dancers,")
+print(str(verb) + " questions cause' they got all the answers.")
